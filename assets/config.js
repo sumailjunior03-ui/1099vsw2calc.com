74aa659d672959cb401bd49ac1c86be8152708c5
sumailjunior03-ui <sumailjunior03@gmail.com>
2026-03-07 05:06:53 +0500

    upsdated

--- assets/config.js
+++ assets/config.js
@@ -15,5 +15,6 @@ export const RELATED_TOOLS = [
   { name: "hourly2salarycalc.com", url: "https://hourly2salarycalc.com/" },
   { name: "PayrollDateChecker.com", url: "https://payrolldatechecker.com/" },
   { name: "FreelanceIncomeCalc.com", url: "https://freelanceincomecalc.com/" },
+  { name: "QuarterlyTaxCalc.com", url: "https://quarterlytaxcalc.com/" },
 ];
 
